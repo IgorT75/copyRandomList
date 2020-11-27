e6f5576477dd852e8e8d7b5aad42330bc12f10fe
Igor <i.trubnikov75@gmail.com>
2020-11-27 12:27:17 -0800

    Adding cloning of random tree

--- main.cpp
+++ main.cpp
@@ -10,6 +10,16 @@ public:
 			val(_val), next(_next), random(_random) { }
 };
 
+// https://leetcode.com/problems/clone-binary-tree-with-random-pointer/
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode *random;
+	TreeNode(int x = 0, TreeNode *left = nullptr, TreeNode *right = nullptr, TreeNode *random = nullptr) :
+		val(x), left(left), right(right), random(random) {}
+};
+
 // https://leetcode.com/problems/copy-list-with-random-pointer
 class Solution {
 	std::unordered_map<Node*, Node*> m;
@@ -29,9 +39,27 @@ public:
 	}
 };
 
+class TreeSolution {
+	std::unordered_map<TreeNode*, TreeNode*> m;
+public:
+	TreeSolution() = default;
+	TreeNode* copyRandomBinaryTree(TreeNode* root) {
+		if (root == nullptr) return nullptr;
+		if (m.find(root) != m.end()) return m[root];
+
+		TreeNode* p = new TreeNode(root->val);
+		m[root] = p;
+
+		p->left = copyRandomBinaryTree(root->left);
+		p->right = copyRandomBinaryTree(root->right);
+		p->random = copyRandomBinaryTree(root->random);
+
+		return p;
+	}
+};
+
 int main() {
 	Solution s;
-
 	Node* n = new Node(7, new Node(13, new Node(11, new Node(10, new Node(1)))));
 	n->random = nullptr;
 	n->next->random = n;
@@ -39,6 +67,9 @@ int main() {
 	n->next->next->next->random = n->next->next;
 	n->next->next->next->next->random = n;
 	auto p = s.copyRandomList(n);
+
+	TreeSolution t;
+
 
 	return 0;
 }
